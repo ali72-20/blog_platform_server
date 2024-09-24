839b6dd0180b3cb22fb3d671a5b0f6cbe073cf61
ali72-20 <as20201118@gmail.com>
2024-09-25 02:13:37 +0300

    add blog services decorator

---
+++ src/main/java/com/example/demo/Services/BlogServicesDecorator.java
@@ -0,0 +1,34 @@
+package com.example.demo.Services;
+
+import com.example.demo.Entity.Blog;
+
+import java.util.List;
+
+public abstract class BlogServicesDecorator implements BlogServices{
+    protected BlogServices blogServices;
+
+    @Override
+    public Blog createBlog(Blog blog) {
+        return blogServices.createBlog(blog);
+    }
+
+    @Override
+    public Blog getBlog(Long id) {
+        return blogServices.getBlog(id);
+    }
+
+    @Override
+    public String deleteBlog(Long id) {
+        return blogServices.deleteBlog(id);
+    }
+
+    @Override
+    public Blog updateBlog(Long id, Blog blog) {
+        return blogServices.updateBlog(id,blog);
+    }
+
+    @Override
+    public List<Blog> getAllBlogs() {
+        return blogServices.getAllBlogs();
+    }
+}
