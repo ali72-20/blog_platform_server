bee533dffc99e8fc42a3ed3bc89519d281f03208
ali72-20 <as20201118@gmail.com>
2024-09-25 02:22:53 +0300

    add decorator config

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo.config;
+
+import com.example.demo.Repo.BlogRepository;
+import com.example.demo.Services.BlogServices;
+import com.example.demo.Services.BlogServicesImpl;
+import com.example.demo.Services.ScienceBlogServices;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public BlogServices blogServices(BlogRepository blogRepository) {
+        return new ScienceBlogServices(new BlogServicesImpl());
+    }
+}
