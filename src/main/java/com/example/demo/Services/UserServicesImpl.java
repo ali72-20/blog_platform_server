8c84ced0df44e562e9fdcc490f943707620b7258
ali72-20 <as20201118@gmail.com>
2024-09-24 15:30:59 +0300

    create user Services class

---
+++ src/main/java/com/example/demo/Services/UserServicesImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.Services;
+
+import com.example.demo.Entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServicesImpl implements UserServices{
+    @Override
+    public User login(String username, String password) {
+        return null;
+    }
+
+    @Override
+    public User register(User user) {
+        return null;
+    }
+}
